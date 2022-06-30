cfc21e6bc2671c2fb2ada9e801fdd9d509cabebb
Erbil Colak <erbilcolak90@gmail.com>
2022-07-01 00:52:35 +0300

    Kdv Hesaplama Programı

---
+++ KdvHesaplama/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //değişkenler
+        double girilenTutar,kdvTutar,kdvliTutar,kdvOran;
+
+        System.out.println("Ücret giriniz : ");
+        girilenTutar = scanner.nextDouble();
+
+
+        // KDV tipini girilen tutara göre belirleyeceğiz.
+        boolean kdvTipi = (0<girilenTutar) && (1001> girilenTutar);
+        kdvOran = kdvTipi ? 0.18 : 0.08;
+
+        kdvTutar = girilenTutar * kdvOran ;
+        kdvliTutar = kdvTutar + girilenTutar;
+
+        System.out.println("Kdv oranı = " + kdvOran);
+        System.out.println("Kdv'li tutar = " + kdvliTutar);
+
+
+
+
+
+
+
+
+    }
+}
