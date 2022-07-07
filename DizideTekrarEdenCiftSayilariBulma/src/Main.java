4df8fc8b20b4717dae08daa5d1f86d498d058ba9
Erbil Colak <erbilcolak90@gmail.com>
2022-07-08 00:05:18 +0300

    Dizide Tekrar Eden Sayıları Bulma

---
+++ DizideTekrarEdenCiftSayilariBulma/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = {11,12,12,11,85,75,44,85,92};
+
+        int[] repeatArray = new int[9];
+
+        for(int i = 0 ; i< array.length; i++){
+            repeatArray[i] = 1;
+        }
+        for(int i = 0; i< array.length ; i++){
+            for(int j = 0 ; j < array.length ; j++){
+                if(i != j && array[i] == array[j]){
+                    repeatArray[i]++;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < array.length ; i++){
+            for(int j = 0; j < array.length ; j++){
+                if(i != j && array[i] == array[j]){
+                    array[j] = 0;
+                }
+            }
+        }
+        System.out.println("Array deki tekrarsız numaralar");
+        for(int t: array){
+            if( t != 0){
+                System.out.print(t);
+                System.out.print(",");
+            }
+        }
+        System.out.println("\n");
+
+        for(int i = 0 ; i< array.length ; i++){
+            if(repeatArray[i] > 1){
+                if(array[i] != 0){
+                    System.out.println(array[i] + " sayı " + repeatArray[i] + " kez tekrar etti ");
+                }
+            }
+        }
+
+
+    }
+}
